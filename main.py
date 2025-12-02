fad41db21d76c6da8499678e9f0a61d16e7ffee5
Carlos Eduardo Felix Junior <110876@PROWAY.TREINA>
2025-12-01 21:58:20 -0300

    Começado projeto.

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.mercado_categoria import executar
+
+
+if __name__ == "__main__":
+    executar()
+
+    
---
+++ src/mercado_categoria.py
@@ -0,0 +1,23 @@
+def executar():
+    # criar_categoria()
+    # listar_categorias()
+    # editar_categorias()
+    # apagar_categorias()
+
+    print("Obrigado!")
+
+
+def criar_categoria():
+    pass
+
+
+def editar_categorias():
+    pass
+
+
+def listar_categorias():
+    pass
+
+
+def apagar_categorias():
+    pass
