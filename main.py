e18feac99645f1871ca3fbbff4569d5a828d3f32
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-12-05 21:36:48 -0300

    Feito biblioteca_manga_repositorio e suas funções.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from src.biblioteca_revista import executar_biblioteca_revista
 if __name__ == "__main__":
     # executar()
 
-    executar_biblioteca_livros()
+    # executar_biblioteca_livros()
 
-    # executar_biblioteca_mangas()
+    executar_biblioteca_mangas()
     
     # executar_biblioteca_revista()
--- src/biblioteca_manga.py
+++ src/biblioteca_manga.py
@@ -1,4 +1,4 @@
-from mysql.connector import connect
+from src.repositorios import biblioteca_manga_repositorio
 
 
 # 14.   MySQL Criar uma tabela de mangas com as colunas: id, nome, volume, autor, data de lançamento
@@ -11,44 +11,25 @@ from mysql.connector import connect
 
 
 def executar_biblioteca_mangas():
-    listar_mangas()
+    # listar_mangas()
     # editar_manga()
-    # apagar_manga()
+    apagar_manga()
     # cadastrar_manga()
 
 
 def listar_mangas():
-    conexao = connect(
-        host="127.0.0.1",
-        port="3306",
-        user="root",
-        password="admin",
-        database="biblioteca",
-    )
+    mangas = biblioteca_manga_repositorio.obter_todos()
 
-    cursor = conexao.cursor()
+    for manga in mangas:
+        id = manga["id"]
 
-    sql = "SELECT id, nome, volume, autor, data_lancamento FROM mangas"
-    
-    cursor.execute(sql)
+        nome = manga["nome"]
 
-    registros = cursor.fetchall()
+        volume = manga["volume"]
 
-    conexao.close()
+        autor = manga["autor"]
 
-    cursor.close()
-
-
-    for registro in registros:
-        id = registro[0]
-
-        nome = registro[1]
-
-        volume = registro[2]
-
-        autor = registro[3]
-
-        data_lancamento = registro[4]
+        data_lancamento = manga["data_lancamento"]
 
         print("ID:", id, "\tNOME:", nome, "\tVOLUME:", volume, "\tAUTOR:", autor, "\tDATA DE LANÇAMENTO:", data_lancamento)
 
@@ -66,29 +47,8 @@ def editar_manga():
 
     data_lancamento = input("Digite a data de lançamento do volume: ")
 
-    conexao = connect(
-        user="root",
-        password="admin",
-        host="127.0.0.1",
-        port="3306",
-        database="biblioteca",
-    )
+    linhas_modificadas = biblioteca_manga_repositorio.editar(id, nome, volume, autor, data_lancamento)
 
-    dados = (nome, volume, autor, data_lancamento, id)
-
-    sql = "UPDATE mangas SET nome=%s, volume= %s, autor= %s, data_lancamento= %s WHERE id= %s"
-
-    cursor = conexao.cursor()
-
-    cursor.execute(sql, dados)
-
-    conexao.commit()
-
-    cursor.close()
-
-    conexao.close()
-
-    linhas_modificadas = cursor.rowcount
     if linhas_modificadas == 0:
         print("Ocorreu um erro ao tentar editar o mangá.")
     else:
@@ -100,29 +60,8 @@ def apagar_manga():
 
     id = input("Digite o ID do mangá que deseja apagar: ")
 
-    conexao = connect(
-        password="admin",
-        user="root",
-        database="biblioteca",
-        port="3306",
-        host="127.0.0.1"
-    )
+    linhas_apagadas = biblioteca_manga_repositorio.apagar(id)
 
-    sql = "DELETE FROM mangas WHERE id = %s"
-
-    dados = (id, )
-
-    cursor = conexao.cursor()
-
-    cursor.execute(sql, dados)
-
-    conexao.commit()
-
-    cursor.close()
-
-    conexao.close()
-
-    linhas_apagadas = cursor.rowcount
     if linhas_apagadas == 0:
         print("Ocorreu um erro ao tentar apagar o mangá.")
     else:
@@ -138,28 +77,7 @@ def cadastrar_manga():
 
     data_lancamento = input("Digite a data de lançamento do volume: ")
 
-    conexao = connect(
-        port="3306",
-        user="root",
-        database="biblioteca",
-        host="127.0.0.1",
-        password="admin"
-    )
-
-    cursor = conexao.cursor()
-
-    sql = "INSERT INTO mangas (nome, volume, autor, data_lancamento) VALUES (%s, %s, %s, %s)"
-
-    dados = (nome, volume, autor, data_lancamento)
-
-    cursor.execute(sql, dados)
-
-    conexao.commit()
-
-    cursor.close()
-
-    conexao.close()
-
+    biblioteca_manga_repositorio.cadastrar(nome, volume, autor, data_lancamento)
 
     print(f"Mangá '{nome}' cadastrado com sucesso!")
 
